7cfcb63a317efef9ea52ed979c9e3727b26af7be
Daniel <medinadaniel53@gmail.com>
2019-10-02 16:30:25 -0500

    new index

---
+++ menu.js
@@ -0,0 +1,41 @@
+var TitleChange = document.getElementById("firstTitle");
+TitleChange.setAttribute("style", "color: purple");
+
+var totalChildren = document.getElementById("navlinks");
+console.log (totalChildren.childNodes.length);
+
+var totalTags = document.getElementById("li");
+console.log ("That are " + totalChildren.length +" menu items on this page.");
+
+var myMain = document.getElementById("main");
+var newPara = document.createElement("p");
+var Paratxt = document.createTextNode("My new paragraph!@#");
+
+myMain.appendChild(newPara);
+newPara.appendChild(Paratxt);
+
+(function(menu_button, links, breakpoint) {
+	'use strict';
+	var menulink = document.getElementById(menu_button),
+	    menu = document.getElementById(links);
+		
+	menu.className = 'start';
+	setTimeout(function() {
+		menu.className = 'collapsed';
+	}, 20);
+		
+	menulink.onclick = function() {
+		if (menu.className === 'displayed') {
+			menu.className = 'collapsed';
+		} else {
+			menu.className = 'displayed';
+		}
+		return false;
+	};
+	
+	window.onresize = function() {
+		if (window.innerWidth < breakpoint) {
+			menu.className = 'collapsed';
+		}
+	};	
+})('menulink', 'navlinks', 700);
